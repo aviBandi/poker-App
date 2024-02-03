23792818ec777c5b005c0e9a0b5d32a917db1bd6
Avi Bandi <bandiavi000@isd284.com>
2024-02-02 23:49:45 -0600

    have the right map and clean data
    this step took me way too long

--- backEnd/main.py
+++ backEnd/main.py
@@ -1,23 +1,40 @@
+hand = ['2H', '3H']
+community = ['KH', '10C', '4H', 'AH', '6S']
 
-# Need to use a string method first to have a string of the card value and suit. Then need to do a card[:1] to get the first digit letter
-def map_card_value(value):
-    if value.isdigit():
-        return int(value)
-    elif value == 'A':
-        return 14
-    elif value == 'K':
-        return 13
-    elif value == 'Q':
-        return 12
-    elif value == 'J':
-        return 11
-    else:
-        raise ValueError("Invalid card value")
+def map_card_value(card):
+    values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+    return values[card[:-1]]
 
+
+# sort the list in numerical order and make all face cards into a numerical value
+def cleanData():
+    global all_cards
+    all_cards = hand + community
+    all_cards.sort(key=lambda x: map_card_value(x))
+    for i, each in enumerate(all_cards):
+        if each[0] in ['J', 'Q', 'K', 'A']:
+            all_cards[i] = str(map_card_value(each)) + each[1]
+    print(all_cards)
+
+    
+cleanData()
+
+
+# print(map_card_value('AH'))
 
 # def check_straight_flush(hand, community):
 #     all_cards = hand + community
-#     all_cards.sort(key=lambda x: map_card_value(x[0]))
+#     all_cards.sort(key=lambda x: map_card_value(x))
+#     print(all_cards)
+#
+# check_straight_flush(hand,community)
+
+
+
+
+
+
+
 #     for i in range(len(all_cards) - 4):
 #         if all_cards[i][1] == all_cards[i+4][1]:
 #             straight_flush = True
@@ -29,6 +46,107 @@ def map_card_value(value):
 #                 return True
 #     return False
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# Need to use a string method first to have a string of the card value and suit. Then need to do a card[:1] to get the first digit letter
+# def map_card_value(value):
+#     if value.isdigit():
+#         return int(value)
+#     elif value == 'A':
+#         return 14
+#     elif value == 'K':
+#         return 13
+#     elif value == 'Q':
+#         return 12
+#     elif value == 'J':
+#         return 11
+#     else:
+#         raise ValueError("Invalid card value")
+#
+
+
+# def custom_key(item):
+#     return int(item[:-1])  # Convert the numeric part to an integer
+# def map_card_value(card):
+#     values = {'2': 2, '3': 3, '4': 4, '5': 5, '6': 6, '7': 7, '8': 8, '9': 9, '10': 10, 'J': 11, 'Q': 12, 'K': 13, 'A': 14}
+#     return str(values[card[:-1]]) + f"{card[-1]}"
+# def check_straight_flush(hand, community):
+#     all_cards = hand + community
+#     all_cards.sort(key=lambda x: map_card_value(x))
+
+#     # for i in range(len(all_cards)-4):
+#     #     if all_cards[i]
+
+
+    # for i in range(len(all_cards) - 4):
+    #     if all_cards[i][1] == all_cards[i+4][1]:
+    #         straight_flush = True
+    #         for j in range(i, i+4):
+    #             if map_card_value(all_cards[j][0]) != map_card_value(all_cards[j+1][0]) - 1:
+    #                 straight_flush = False
+    #                 break
+    #         if straight_flush:
+    #             return True
+    # return False
+
+# print(check_straight_flush(hand, community))
+
+# hand = ['10H', '8D']
+# community = ['7S', '10C', '8H', '7D', '6S']
+#
+# def check_straight_flush(hand, community):
+#     all_cards = hand + community
+#     all_cards.sort(key=lambda x: map_card_value(x[0]))
+#     print(all_cards)
+    # for i in range(len(all_cards) - 4):
+    #     if all_cards[i][1] == all_cards[i+4][1]:
+    #         straight_flush = True
+    #         for j in range(i, i+4):
+    #             if map_card_value(all_cards[j][0]) != map_card_value(all_cards[j+1][0]) - 1:
+    #                 straight_flush = False
+    #                 break
+    #         if straight_flush:
+    #             return True
+    # return False
+
+
+# check_straight_flush(hand, community)
 
 #
 # def check_four_of_a_kind(hand, community):
@@ -126,22 +244,22 @@ def map_card_value(value):
 #     else:
 #         return "High Card"
 #
-def main():
-    community_cards = []
-    for i in range(5):
-        card = input(f"Enter community card {i+1} (e.g., '10H' for 10 of Hearts): ").upper()
-        # community_cards.append((int(card[:-1]), card[-1]))
-        community_cards.append(card)
-
-    player_hand = []
-    for i in range(2):
-        card = input(f"Enter your hand card {i+1} (e.g., '10H' for 10 of Hearts): ").upper()
-        player_hand.append(card)
-        # player_hand.append((int(card[:-1]), card[-1]))
-
-    # hand_rank = evaluate_hand(player_hand, community_cards)
-    # print(f"\nYour hand is: {hand_rank}")
-    print(community_cards)
-    print(player_hand)
-if __name__ == "__main__":
-    main()
+# def main():
+#     community_cards = []
+#     for i in range(5):
+#         card = input(f"Enter community card {i+1} (e.g., '10H' for 10 of Hearts): ").upper()
+#         # community_cards.append((int(card[:-1]), card[-1]))
+#         community_cards.append(card)
+#
+#     player_hand = []
+#     for i in range(2):
+#         card = input(f"Enter your hand card {i+1} (e.g., '10H' for 10 of Hearts): ").upper()
+#         player_hand.append(card)
+#         # player_hand.append((int(card[:-1]), card[-1]))
+#
+#     # hand_rank = evaluate_hand(player_hand, community_cards)
+#     # print(f"\nYour hand is: {hand_rank}")
+#     print(community_cards)
+#     print(player_hand)
+# if __name__ == "__main__":
+#     main()
