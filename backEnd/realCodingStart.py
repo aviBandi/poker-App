71c1d4083ad4b6f8c22187682c65beb28bfe321c
aviBandi <bandiavi000@isd284.com>
2024-02-23 14:31:26 -0600

    shows the hand name now after computation
    this is for easiness of dev.

--- backEnd/realCodingStart.py
+++ backEnd/realCodingStart.py
@@ -3,12 +3,21 @@ import computationClass
 river_image = "river.jpg"
 hand_image = "hand.jpg"
 
+handNames = {8: "Four of a Kind",
+             7: "Full House",
+             6: "Flush",
+             5: "Straight",
+             4: "Three of a Kind",
+             3: "Two Pair",
+             2: "Pair",
+             1: "High Card"}
+
 print("river cards")
 detectIMGOnly.cardsFromImage(river_image)
 
 print("hand cards")
 detectIMGOnly.cardsFromImage(hand_image)
 
-computation = computationClass.Computation()
+computation = computationClass.Computation(detectIMGOnly.cardsFromImage(hand_image), detectIMGOnly.cardsFromImage(river_image))
 computation.cleanData()
-print(computation.compute_hand())
+print(handNames[computation.compute_hand()])
